dfd3cec9f4b9af5a9740cffc8d6cd14cead43e3f
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-06-13 20:53:09 +1000

    Expense types are loaded from array of objects

--- script.js
+++ script.js
@@ -6,28 +6,20 @@ const addTransactionForm = document.getElementById('add-transaction-form');
 const transactTextEl = document.getElementById('transact-text');
 const transactAmountEl = document.getElementById('transact-amount');
 const balanceEl = document.getElementById('balance');
+const radioContainerEl = document.getElementById('radio');
 
-//Initialise arrays with transaction name and values:
-const database = [
-  {
-    description:"Income1",
-    amount:4,
-  },
-  {
-    description:"Income1",
-    amount:-3,
-  }
-]
+//Initialise arrays to be connected to database later
+const database = [];
 
 //Function to update account summary (income/expenses)
 function updateAccount() {
   //Update summary income / expense amounts
   incomeTot = database
     .filter((db) => parseFloat(db.amount) >= 0)
-    .reduce((sum, record) => sum + record.amount,0);
+    .reduce((sum, record) => sum + record.amount, 0);
   expensesTot = database
     .filter((db) => parseFloat(db.amount) < 0)
-    .reduce((sum, record) => sum + record.amount,0);
+    .reduce((sum, record) => sum + record.amount, 0);
   incomeEl.innerHTML = `<h3>INCOME:</h3><p>${incomeTot}</p>`;
   expensesEl.innerHTML = `<h3>EXPENSES:</h3><p>${expensesTot}</p>`;
 
@@ -59,13 +51,79 @@ addTransactionForm.onsubmit = addTransaction;
 //Function to add transactions to the array, and rerun the update function.
 function addTransaction(event) {
   event.preventDefault();
-  database.push(  {
-    description:transactTextEl.value,
-    amount:parseFloat(transactAmountEl.value),
-  })
+  database.push({
+    description: transactTextEl.value,
+    amount: parseFloat(transactAmountEl.value),
+  });
   // descriptionArr.push(transactTextEl.value);
   // amountsArr.push(parseFloat(transactAmountEl.value));
   updateAccount();
 }
 
+const expenseItems = [
+  {
+    value: 'Electricity/Gas',
+    id: 'elecGas',
+  },
+  {
+    value: 'Mobile',
+    id: 'mobile',
+  },
+  {
+    value: 'Internet',
+    id: 'internet',
+  },
+  {
+    value: 'Water',
+    id: 'water',
+  },
+  {
+    value: 'Groceries',
+    id: 'groceries',
+  },
+  {
+    value: 'Health Insurance',
+    id: 'healthInsurance',
+  },
+];
+
+// Function to add expense types to the array, re-update the sidebar.
+// TODO implement adding expenses
+
+//Function to set multiple attributes at once. Object.entries in EC8 allows not having to lookup one of key or value by index
+// https://stackoverflow.com/questions/12274748/setting-multiple-attributes-for-an-element-at-once-with-javascript#comment119120551_12274782
+function setAttributes(el, attrs) {
+  Object.entries(attrs).forEach(([key, value]) => el.setAttribute(key, value));
+}
+
+function createExpenseType(expense) {
+  expenseInputEl = document.createElement('input');
+  setAttributes(expenseInputEl, {
+    class: 'radio__input',
+    value: expense.value,
+    type: 'radio',
+    name: 'expense_type',
+    id: expense.id,
+  });
+  expenseLabelEl = document.createElement('label');
+  setAttributes(expenseLabelEl, {
+    for: expense.id,
+    class: 'radio__label',
+  });
+  expenseLabelEl.innerText = expense.value;
+  return {
+    expenseInputEl,
+    expenseLabelEl,
+  };
+}
+
+function updateExpenseType() {
+  expenseItems.forEach((expense) => {
+    let expenseItem = createExpenseType(expense);
+    radioContainerEl.appendChild(expenseItem.expenseInputEl);
+    radioContainerEl.appendChild(expenseItem.expenseLabelEl);
+  });
+}
+
+updateExpenseType();
 updateAccount();
