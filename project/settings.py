afdad50afba55f1fb7975e4387680b9914124f06
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-07-05 21:50:09 +1000

    Add django-environ and set up env variables for secret-key and debug mode

--- project/settings.py
+++ project/settings.py
@@ -10,22 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 import django_heroku
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = environ.Env(DEBUG=(bool, False))
+
+# read the .env file
+env_file = os.path.join(BASE_DIR, ".env")
+environ.Env.read_env(env_file)
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tyjg%kqicng2n0296&kif_k578n-ls2jjdl*#%yi6%tgl-dei)"
+# Secret key kept in .env file
+SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# If I run it locally Debug will be set to False
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
