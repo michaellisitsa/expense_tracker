74c0b8a58a4f25f3575c108efc7298bd9a4acebc
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-07-06 22:11:11 +1000

    Add django allauth dependency and add to settings file

--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "expensetracker.core",
+    # 3rd Party
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",  # new
+    "allauth.socialaccount",  # new
     # local
     "expensetracker.users",
 ]
@@ -152,3 +157,22 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Django heroku modifies the above settings in place by modifying the locals.
 django_heroku.settings(locals(), secret_key=False)
+
+# config/settings.py
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_UNIQUE_EMAIL = True
