8283d0ae6b6657312ba8c81ad45f20403de475ce
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-07-12 21:44:00 +1000

    Display all entries in DB in HTML template - Temporarily disable JS functionality so the cards can be displayed, otherwise they are instantly overridden by JS - Allow form submission by not redirecting on submit (remove action, and remove return in the app view

--- expensetracker/core/views.py
+++ expensetracker/core/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from .models import Expenses
@@ -14,12 +14,15 @@ class IndexView(TemplateView):
 # redirect to a login page or display an error rather than take you to the "app.html" page
 @login_required
 def app(request):
-    filtered_description = Expenses.objects.filter(id=1)
+    filtered_description = Expenses.objects.all()
+    # expense_instance = get_object_or_404(Expenses, id=1)
     if request.method == "POST":
         # Create a form instance and populate with data from the request
         form = ExpensesForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect("/submitted/")
+            # expense_instance.description = form.cleaned_data["description"]
+            form.save()
+            # return HttpResponseRedirect("/submitted/")
     else:
         form = ExpensesForm()
     context = {"filtered_description": filtered_description, "form": form}
