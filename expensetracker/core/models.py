c0debc165922403889f7227bae87f02e0ba28955
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-07-27 22:08:36 +1000

    Add django models for expense, ExpenseTimePeriod, ExpenseCategory

--- expensetracker/core/models.py
+++ expensetracker/core/models.py
@@ -17,3 +17,28 @@ class Expenses(models.Model):
 
     def __str__(self):
         return f"{self.description} ${self.cost:.2f}"
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=100)
+    assignee = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    groupTransactions = models.BooleanField(default=True)
+    
+
+# Expenses are grouped into time periods, related by a foreign key
+class ExpenseTimePeriod(models.Model):
+    description = models.CharField(max_length=100)
+    dateStart = models.DateField('dateStart')
+    dateEnd = models.DateField('dateEnd')
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.description} {self.dateStart} to {self.dateEnd}"
+
+class Expense(models.Model):
+    description = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+    expenseTimePeiod = models.ForeignKey(ExpenseTimePeriod, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.description} ${self.cost:.2f}"
