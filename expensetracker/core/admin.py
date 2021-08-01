57c87ff2a452e9de3c26e0665473ab0436f1641c
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-08-01 21:41:22 +1000

    Add model tables to admin

--- expensetracker/core/admin.py
+++ expensetracker/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import ExpenseTimePeriod, Expense, ExpenseCategory
+
+admin.site.register(ExpenseCategory)
+admin.site.register(ExpenseTimePeriod)
+admin.site.register(Expense)
