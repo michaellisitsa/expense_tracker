e95ff94a659270c981cefc18188c088441aadac0
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-08-03 23:12:39 +1000

    Add filters to expense time period page. Temp remove create form due to duplicate ids

--- expensetracker/core/views.py
+++ expensetracker/core/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from .models import ExpenseTimePeriod, ExpenseCategory, Expense
 from .forms import CategoryForm, ExpenseTimePeriodForm, ExpenseForm
+from .filters import ExpenseTimePeriodFilter
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -42,31 +43,37 @@ def time_period (request, pk=1):
     """
     timePeriodPerCategory = ExpenseTimePeriod.objects.filter(category__pk=pk)
     expenseCategory = ExpenseCategory.objects.get(id=pk)
-    if request.method == "POST":
-        # Create a form instance and populate with data from the request
-        form = ExpenseTimePeriodForm(request.POST)
-        if form.is_valid():
-            # You can obtain the id of the record just created, and then pass it into the url inside HttpResponseRedirect
-            # https://stackoverflow.com/questions/41700796/how-to-get-id-in-the-url-after-submission-of-form
-            instance = form.save(commit = False)
-            instance.user = ExpenseTimePeriod.objects.get(id=request.user.id)
-            instance.save()            
-            messages.success(request, "Expense Time Period submitted successfully.")
-            # When using HttpResponseRedirect, remove action from form template
-            # https://stackoverflow.com/a/60816124/12462631
-            # You can use kwargs to pass into the url string. Refer example from
-            # https://docs.djangoproject.com/en/3.2/ref/urlresolvers/
-            # >>> reverse('admin:app_list', kwargs={'app_label': 'auth'})
-            # '/admin/auth/'
-            return HttpResponseRedirect(reverse('core:createExpensesSelected',kwargs={'pk': instance.id}))
-        else:
-            messages.error(request, "Invalid form submission.")
-    else:
-        form = ExpenseTimePeriodForm(initial={'category':expenseCategory})
+    # if request.method == "POST":
+    #     # Create a form instance and populate with data from the request
+    #     form = ExpenseTimePeriodForm(request.POST)
+    #     if form.is_valid():
+    #         # You can obtain the id of the record just created, and then pass it into the url inside HttpResponseRedirect
+    #         # https://stackoverflow.com/questions/41700796/how-to-get-id-in-the-url-after-submission-of-form
+    #         instance = form.save(commit = False)
+    #         instance.user = ExpenseTimePeriod.objects.get(id=request.user.id)
+    #         instance.save()            
+    #         messages.success(request, "Expense Time Period submitted successfully.")
+    #         # When using HttpResponseRedirect, remove action from form template
+    #         # https://stackoverflow.com/a/60816124/12462631
+    #         # You can use kwargs to pass into the url string. Refer example from
+    #         # https://docs.djangoproject.com/en/3.2/ref/urlresolvers/
+    #         # >>> reverse('admin:app_list', kwargs={'app_label': 'auth'})
+    #         # '/admin/auth/'
+    #         return HttpResponseRedirect(reverse('core:createExpensesSelected',kwargs={'pk': instance.id}))
+    #     else:
+    #         messages.error(request, "Invalid form submission.")
+    # else:
+    #     form = ExpenseTimePeriodForm(initial={'category':expenseCategory})
+    
+    #Re-instantiate that variable with the filtered query set using Django-filter
+    myFilter = ExpenseTimePeriodFilter(request.GET, queryset=timePeriodPerCategory)
+    timePeriodPerCategory = myFilter.qs
+
     context = {
         "expenses": timePeriodPerCategory,
-        "form": form,
-        "expenseCategory":expenseCategory,
+        # "form": form,
+        "expenseCategory": expenseCategory,
+        'myFilter': myFilter,
     }
     return render(request, "core/timePeriod.html", context)
 
