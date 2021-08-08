287e914097e5440f7d138d9fd7f703d61c8e4a5a
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-08-08 22:53:14 +1000

    Add fetch post to expense category form

--- expensetracker/core/static/app/script.js
+++ expensetracker/core/static/app/script.js
@@ -6,6 +6,7 @@
 */
 
 expensePeriodForm = document.querySelector("#ExpensePeriodForm")
+asyncBtn = document.querySelector("#asyncBtn")
 
 // expensePeriodForm.onsubmit = submitForm
 
@@ -19,4 +20,43 @@ function submitForm (e) {
     console.log(data);
     axios.post('https://jsonplaceholder.typicode.com/posts', data)
     .then(res => console.log(res.request.response))
+}
+
+data = JSON.stringify({
+    name: 'API Category',
+    assignee: 'API Assignee',
+    description: 'API description',
+    groupTransactions: true,
+})
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+if (asyncBtn) {
+    asyncBtn.onclick = asyncFormSubmit
+}
+function asyncFormSubmit (e) {
+    fetch('http://0.0.0.0:5000/api/expenseCategory/', {
+    method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': "application/json",
+        'X-CSRFToken': csrftoken 
+    },
+    body: data
+    }).then(res=>res.json())
+      .then(res => console.log(res));
 }
