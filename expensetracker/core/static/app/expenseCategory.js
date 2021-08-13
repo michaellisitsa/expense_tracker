7e3904f44064755723d6fd1d05c94577ebabadb0
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-08-13 17:06:04 +1000

    Split js file for each template, remove old code

--- expensetracker/core/static/app/expenseCategory.js
+++ expensetracker/core/static/app/expenseCategory.js
@@ -7,26 +7,7 @@
 
 expensePeriodForm = document.querySelector("#ExpensePeriodForm")
 asyncBtn = document.querySelector("#asyncBtn")
-asyncBtn = document.querySelector("#asyncDeleteBtn")
-
-let expenseFormset = document.querySelectorAll(".expense-formset")
-let container = document.querySelector("#form-container")
-let addButton = document.querySelector("#add-form")
-let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
-
-// expensePeriodForm.onsubmit = submitForm
-
-function submitForm (e) {
-    e.preventDefault();
-    const formData = new FormData(this); // reference to form element
-    const data = {}; // need to convert it before using not with XMLHttpRequest
-    for (let [key, val] of formData.entries()) {
-        Object.assign(data, { [key]: val })
-      }
-    console.log(data);
-    axios.post('https://jsonplaceholder.typicode.com/posts', data)
-    .then(res => console.log(res.request.response))
-}
+asyncDeleteBtn = document.querySelector("#asyncDeleteBtn")
 
 data = JSON.stringify({
     name: 'API Category',
@@ -53,9 +34,7 @@ function getCookie(name) {
     return cookieValue;
 }
 const csrftoken = getCookie('csrftoken');
-if (asyncBtn) {
-    asyncBtn.onclick = asyncFormSubmit
-}
+asyncBtn.onclick = asyncFormSubmit
 
 // Making a post request
 // Stack Overflow:
@@ -73,14 +52,11 @@ function asyncFormSubmit (e) {
       .then(res => console.log(res));
 }
 
+asyncDeleteBtn.onclick = asyncFormDelete
+
 // Making a delete request
-
-if (asyncDeleteBtn) {
-    asyncDeleteBtn.onclick = asyncFormDelete
-}
-
 function asyncFormDelete (e) {
-    fetch('http://0.0.0.0:5000/api/expenseCategory/4', {
+    fetch('http://0.0.0.0:5000/api/expenseCategory/3', {
     method: 'delete',
       headers: {
         'Accept': 'application/json, text/plain, */*',
@@ -89,23 +65,4 @@ function asyncFormDelete (e) {
     },
     }).then(res=>res.text())
       .then(res => console.log(res));
-}
-
-// Functionality for adding extra entries into the formset
-let formNum = expenseFormset.length - 1 // Get the num of the last form on the page
-
-addButton && addButton.addEventListener('click',addForm)
-
-function addForm(e) {
-    e.preventDefault()
-
-    let newForm = expenseFormset[0].cloneNode(true)  // Clone the last form
-    let formRegex = RegExp(`form-(\\d){1}-`,'g') // Regex to find all instances of the form number
-
-    formNum++ // Increment the form number
-    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
-    container.insertBefore(newForm, addButton)
-
-    totalForms.setAttribute('value',`${formNum+1}`) // Increment the total number of forms in the hidden input 
-
 }
