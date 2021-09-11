a311bb68a9e9c87eec500503ce22dfc94276fc48
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-09-11 22:45:07 +1000

    Fixed incorrect filter, add hyperlinks on home page

--- expensetracker/core/filters.py
+++ expensetracker/core/filters.py
@@ -5,23 +5,28 @@ from django.forms import ModelForm, RadioSelect, DateInput, Select, CheckboxInpu
 
 from .models import *
 
+
 def categoryFilter(request):
     if request is None:
         return ExpenseCategory.objects.none()
 
     return ExpenseCategory.objects.filter(user=request.user)
+
+
 class ExpenseTimePeriodFilter(django_filters.FilterSet):
     # Link describing how to set up filters, including the lte gte etc. styles
     # https://youtu.be/G-Rct7Na0UQ?t=809
-    category = ModelChoiceFilter(queryset=categoryFilter,widget=RadioSelect(attrs={'id':'categoryField','class':'categoryFilter'}))
+    category = ModelChoiceFilter(
+        queryset=categoryFilter,
+        widget=RadioSelect(attrs={"id": "categoryField", "class": "categoryFilter"}),
+    )
     # startAfter = DateFilter(field_name='dateStart', lookup_expr='gte',widget=DateInput(format=('%d/%m/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}))
     # startBefore = DateFilter(field_name='dateStart', lookup_expr='lte',widget=DateInput(format=('%d/%m/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}))
     # note = CharFilter(field_name='description',lookup_expr='icontains')
-    
-    
+
     class Meta:
         model = ExpenseTimePeriod
-        fields = ['category','startDate']
+        fields = ["category"]
 
     # def __init__(self, user, *args, **kwargs):
     #     super(ExpenseTimePeriodFilter, self).__init__(*args, **kwargs)
@@ -29,6 +34,6 @@ class ExpenseTimePeriodFilter(django_filters.FilterSet):
     # @property
     # def qs(self):
     #     queryset=super(ExpenseTimePeriodFilter, self).qs
-    #     if request.user.has_perm("app_label.has_permission"):       
+    #     if request.user.has_perm("app_label.has_permission"):
     #         return queryset.exclude(invited_user!=self.request.user)
-    #     return queryset      
+    #     return queryset
