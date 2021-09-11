e879c3f4dfe4a48b0873632f7c161c72b9795054
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-09-11 22:24:07 +1000

    Styling signup, login page

--- expensetracker/users/views.py
+++ expensetracker/users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Overriding default styling of user forms
+# https://gavinwiener.medium.com/modifying-django-allauth-forms-6eb19e77ef56
