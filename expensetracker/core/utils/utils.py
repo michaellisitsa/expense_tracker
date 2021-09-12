ac1d8492f29731d78b6b63ec791d6fe042e91e2d
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2021-09-12 21:35:42 +1000

    Fix sum query trying to turn NoneType into float

--- expensetracker/core/utils/utils.py
+++ expensetracker/core/utils/utils.py
@@ -1,12 +1,15 @@
 from datetime import datetime, timedelta
 from django.db.models import Q, Sum
 
-#Utility to generate Summary table
+# Utility to generate Summary table
 def summariseTimePeriod(time_periods, expenses, days):
     currentDate = datetime.now().date()
     dayDelta = timedelta(days)
     prevDate = currentDate - dayDelta
-    filtered_time_periods = time_periods.filter(Q(dateStart__range=[prevDate, currentDate]) | Q(dateEnd__range=[prevDate, currentDate]))
+    filtered_time_periods = time_periods.filter(
+        Q(dateStart__range=[prevDate, currentDate])
+        | Q(dateEnd__range=[prevDate, currentDate])
+    )
 
     # Get number of days within last week/month/year falling into a time period
     adjusted_dayDelta = []
@@ -14,20 +17,33 @@ def summariseTimePeriod(time_periods, expenses, days):
     period_id = []
     adjusted_expense = []
     for time_period in filtered_time_periods:
-        total_expenses = float(expenses.filter(expenseTimePeriod=time_period).aggregate(Sum('cost'))['cost__sum']) or 0
+        total_expenses = (
+            expenses.filter(expenseTimePeriod=time_period).aggregate(Sum("cost"))[
+                "cost__sum"
+            ]
+            or 0
+        )
+        total_expense = float(total_expenses)
         if time_period.dateStart < prevDate:
-            adjusted_dayDelta.append((time_period.dateEnd-prevDate).days)
-            adjusted_expense.append((time_period.dateEnd-prevDate).days/(time_period.dateEnd-time_period.dateStart).days * total_expenses)
+            adjusted_dayDelta.append((time_period.dateEnd - prevDate).days)
+            adjusted_expense.append(
+                (time_period.dateEnd - prevDate).days
+                / (time_period.dateEnd - time_period.dateStart).days
+                * total_expenses
+            )
         else:
-            adjusted_dayDelta.append((time_period.dateEnd-time_period.dateStart).days)
+            adjusted_dayDelta.append((time_period.dateEnd - time_period.dateStart).days)
             adjusted_expense.append(float(total_expenses))
-        orig_dayDelta.append((time_period.dateEnd-time_period.dateStart).days)
+        orig_dayDelta.append((time_period.dateEnd - time_period.dateStart).days)
         period_id.append(time_period.id)
-            
-    # Convert to the average expenses per month. 
-    expenses_rate = int(30. / sum(adjusted_dayDelta) * sum(adjusted_expense)) if sum(adjusted_dayDelta) != 0 else "-"
+
+    # Convert to the average expenses per month.
+    expenses_rate = (
+        int(30.0 / sum(adjusted_dayDelta) * sum(adjusted_expense))
+        if sum(adjusted_dayDelta) != 0
+        else "-"
+    )
     # TODO - Remove overlapping days
     coverage = int(sum(adjusted_dayDelta) / days * 100)
-
 
     return (expenses_rate, coverage)
