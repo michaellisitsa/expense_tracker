d43721280b6fdb72aad2c0b993d2fce1d4ce02df
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2021-11-06 18:59:31 +1100

    Move fetch requsts and buttons to Form component

--- reactapp/src/CategoryForm.jsx
+++ reactapp/src/CategoryForm.jsx
@@ -1,12 +1,60 @@
 import React, { Component } from "react";
 import "./CategoryForm.css";
+import { getCookie } from "./utils/cookieUtils";
 
 export class CategoryForm extends Component {
   state = {
     number1: "0",
     number2: "0",
     operator: "+",
+    csrftoken: "unset",
+    postData: JSON.stringify({
+      name: "API Category",
+      assignee: "API Assignee",
+      budget: "100",
+      description: "API description",
+      user: "1",
+    }),
   };
+
+  // Making a post request
+  // Stack Overflow:
+  // https://stackoverflow.com/questions/45308153/posting-data-to-django-rest-framework-using-javascript-fetch
+  asyncFormSubmit = (e) => {
+    fetch("/api/expenseCategory/", {
+      method: "post",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        "X-CSRFToken": this.state.csrftoken,
+      },
+      body: this.state.postData,
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
+
+  asyncFormGet = () => {
+    fetch("/api/expenseCategory/", {
+      method: "get",
+    })
+      .then((res) => res.json())
+      .then((res) => console.table(res.results));
+  };
+
+  postRequest = (e) => {
+    e.preventDefault();
+    this.asyncFormSubmit();
+  };
+
+  getRequest = (e) => {
+    e.preventDefault();
+    this.asyncFormGet();
+  };
+
+  componentDidMount() {
+    this.setState({ csrftoken: getCookie("csrftoken") });
+  }
 
   handleChange = (event) => {
     this.setState(
@@ -56,6 +104,12 @@ export class CategoryForm extends Component {
             onChange={this.handleChange}
           />
         </fieldset>
+        <button className="post-request" onClick={this.postRequest}>
+          Post expenseCategory
+        </button>
+        <button className="get-request" onClick={this.getRequest}>
+          Get expenseCategory
+        </button>
       </form>
     );
   }
--- reactapp/src/ExpenseTracker.jsx
+++ reactapp/src/ExpenseTracker.jsx
@@ -2,44 +2,10 @@ import "./ExpenseTracker.css";
 import React from "react";
 import CategoryForm from "./CategoryForm";
 import Output from "./Output";
-import { getCookie } from "./utils/cookieUtils";
 
 class ExpenseTracker extends React.Component {
   state = {
     outputEquation: "",
-    csrftoken: "unset",
-    postData: JSON.stringify({
-      name: "API Category",
-      assignee: "API Assignee",
-      budget: "100",
-      description: "API description",
-      user: "1",
-    }),
-  };
-
-  // Making a post request
-  // Stack Overflow:
-  // https://stackoverflow.com/questions/45308153/posting-data-to-django-rest-framework-using-javascript-fetch
-  asyncFormSubmit = (e) => {
-    fetch("/api/expenseCategory/", {
-      method: "post",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-        "X-CSRFToken": this.state.csrftoken,
-      },
-      body: this.state.postData,
-    })
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-  };
-
-  asyncFormGet = () => {
-    fetch("/api/expenseCategory/", {
-      method: "get",
-    })
-      .then((res) => res.json())
-      .then((res) => console.log(res));
   };
 
   setOutput = (number1, number2, operator) => {
@@ -63,20 +29,7 @@ class ExpenseTracker extends React.Component {
     });
   };
 
-  postRequest = () => {
-    this.asyncFormSubmit();
-  };
-
-  getRequest = () => {
-    this.asyncFormGet();
-  };
-
-  componentDidMount() {
-    this.setState({ csrftoken: getCookie("csrftoken") });
-  }
-
   render() {
-    console.log(this.state.csrftoken);
     return (
       <div className="calculator-wrapper">
         <header>
@@ -84,12 +37,6 @@ class ExpenseTracker extends React.Component {
         </header>
         <CategoryForm setOutput={this.setOutput} />
         <Output output={this.state.outputEquation} />
-        <button className="post-request" onClick={this.postRequest}>
-          Post expenseCategory
-        </button>
-        <button className="get-request" onClick={this.getRequest}>
-          Get expenseCategory
-        </button>
       </div>
     );
   }
