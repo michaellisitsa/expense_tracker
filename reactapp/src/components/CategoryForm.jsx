cf0d02267b883a299e801b9bdfb2cb472a3746d6
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2021-12-11 22:37:04 +1100

    Refactor CategoryForm to hooks

--- reactapp/src/components/CategoryForm.jsx
+++ reactapp/src/components/CategoryForm.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import "./CategoryForm.css";
 import { getCookie } from "../utils/cookieUtils";
 
-export class CategoryForm extends Component {
-  state = {
+function CategoryForm(props) {
+  const [formData, setFormData] = useState({
     name: "",
     assignee: "",
     budget: "",
     description: "",
-    csrftoken: "unset",
-  };
+  });
+  const [csrfToken, setCsrfToken] = useState("unset");
 
   // Making a post request
   // Stack Overflow:
   // https://stackoverflow.com/questions/45308153/posting-data-to-django-rest-framework-using-javascript-fetch
-  asyncFormSubmit = (e) => {
-    const { name, assignee, budget, description } = this.state;
+  const asyncFormSubmit = (e) => {
+    const { name, assignee, budget, description } = formData;
     fetch("/api/expenseCategory/", {
       method: "post",
       headers: {
         Accept: "application/json, */*",
         "Content-Type": "application/json",
-        "X-CSRFToken": this.state.csrftoken,
+        "X-CSRFToken": csrfToken,
       },
       body: JSON.stringify({
         name,
@@ -34,7 +34,7 @@ export class CategoryForm extends Component {
       .then((res) => console.log(res));
   };
 
-  asyncFormGet = () => {
+  const asyncFormGet = () => {
     fetch("/api/expenseCategory/", {
       method: "get",
     })
@@ -42,80 +42,86 @@ export class CategoryForm extends Component {
       .then((res) => console.table(res.results));
   };
 
-  postRequest = (e) => {
+  const postRequest = (e) => {
     e.preventDefault();
-    this.asyncFormSubmit();
+    asyncFormSubmit();
   };
 
-  getRequest = (e) => {
+  const getRequest = (e) => {
     e.preventDefault();
-    this.asyncFormGet();
+    asyncFormGet();
   };
 
-  componentDidMount() {
-    this.setState({ csrftoken: getCookie("csrftoken") });
-  }
+  useEffect(() => {
+    setCsrfToken(getCookie("csrftoken"));
+  }, []);
 
-  handleChange = (event) => {
-    this.setState({
+  // componentDidMount() {
+  //   this.setState({ csrfToken: getCookie("csrfToken") });
+  // }
+
+  const handleChange = (event) => {
+    setFormData((data) => ({
+      ...data,
       [event.target.name]: event.target.value,
-    });
+    }));
+    // this.setState({
+    //   [event.target.name]: event.target.value,
+    // });
   };
 
-  render() {
-    return (
-      <form className="form1" id="form1">
-        <fieldset className="inputs-wrapper">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="category-input"
-            placeholder="Enter Name..."
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="assignee">Assignee:</label>
-          <input
-            type="text"
-            name="assignee"
-            id="assignee"
-            className="category-input"
-            placeholder="Enter Assignee..."
-            value={this.state.assignee}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="budget">Budget:</label>
-          <input
-            type="text"
-            name="budget"
-            id="budget"
-            className="category-input"
-            placeholder="Enter Budget..."
-            value={this.state.budget}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="description">Description:</label>
-          <input
-            type="text"
-            name="description"
-            id="description"
-            className="category-input"
-            placeholder="Enter Description..."
-            value={this.state.description}
-            onChange={this.handleChange}
-          />
-        </fieldset>
-        <button className="post-request" onClick={this.postRequest}>
-          Post expenseCategory
-        </button>
-        <button className="get-request" onClick={this.getRequest}>
-          Get expenseCategory
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className="form1" id="form1">
+      <fieldset className="inputs-wrapper">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="category-input"
+          placeholder="Enter Name..."
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="assignee">Assignee:</label>
+        <input
+          type="text"
+          name="assignee"
+          id="assignee"
+          className="category-input"
+          placeholder="Enter Assignee..."
+          value={formData.assignee}
+          onChange={handleChange}
+        />
+        <label htmlFor="budget">Budget:</label>
+        <input
+          type="text"
+          name="budget"
+          id="budget"
+          className="category-input"
+          placeholder="Enter Budget..."
+          value={formData.budget}
+          onChange={handleChange}
+        />
+        <label htmlFor="description">Description:</label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          className="category-input"
+          placeholder="Enter Description..."
+          value={formData.description}
+          onChange={handleChange}
+        />
+      </fieldset>
+      <button className="post-request" onClick={postRequest}>
+        Post expenseCategory
+      </button>
+      <button className="get-request" onClick={getRequest}>
+        Get expenseCategory
+      </button>
+    </form>
+  );
 }
 
 export default CategoryForm;
---
+++ reactapp/src/components/ExpensePeriodForm.jsx
@@ -0,0 +1,9 @@
+import "./ExpensePeriodForm.css";
+
+import React from "react";
+
+function ExpensePeriodForm(props) {
+  return <div></div>;
+}
+
+export default ExpensePeriodForm;
