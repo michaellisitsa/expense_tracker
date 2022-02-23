a7683dc2046939a3d3beb1e3904e4c7a7c06e475
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2022-02-23 21:16:27 +1100

    add favicon and update title

--- reactapp/src/components/App.jsx
+++ reactapp/src/components/App.jsx
@@ -3,8 +3,16 @@ import ExpenseTracker from "./ExpenseTracker";
 import { HashRouter as Router } from "react-router-dom";
 import TopNav from "./TopNav";
 import "./App.css";
+import icon from "../Assets/favicon.ico";
 
 export default function App(props) {
+  // App.js
+
+  React.useEffect(() => {
+    const favicon = document.getElementById("favicon");
+    favicon.setAttribute("href", icon);
+  }, []);
+
   return (
     <div>
       <Router>
