fe1a0d5ccdd75d33789cc45971efb4dc5c5cb479
Michael Lisitsa <michaellisitsa@192-168-1-132.tpgi.com.au>
2022-02-26 22:05:21 +1100

    Split Uploader components into separate files

--- reactapp/src/components/import/ImportPage.jsx
+++ reactapp/src/components/import/ImportPage.jsx
@@ -1,94 +1,22 @@
 import React from "react";
-import { useCSVReader } from "react-papaparse";
-import { parse } from "../../components/import/ofx-js";
-
-const styles = {
-  csvReader: {
-    display: "flex",
-    flexDirection: "row",
-    marginBottom: 10,
-  },
-  browseFile: {
-    width: "20%",
-  },
-  acceptedFile: {
-    border: "1px solid #ccc",
-    height: 45,
-    lineHeight: 2.5,
-    paddingLeft: 10,
-    width: "80%",
-  },
-  remove: {
-    borderRadius: 0,
-    padding: "0 20px",
-  },
-  progressBarBackgroundColor: {
-    backgroundColor: "red",
-  },
-};
+import CSVUpload from "./CSVUpload";
+import OFXUpload from "./OFXUpload";
 
 function ImportPage(props) {
-  const [csvData, setCsvData] = React.useState([]);
   const [ofxData, setOfxData] = React.useState([]);
+  const [csvData, setCsvData] = React.useState([]);
   const [errors, setErrors] = React.useState([]);
-  const { CSVReader } = useCSVReader();
-
-  function handleOFXImport(event) {
-    event.preventDefault();
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      parse(event.target.result).then((ofxData) => {
-        setOfxData(
-          ofxData.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN
-        );
-      });
-    };
-    reader.readAsText(event.target.files[0]);
-  }
 
   return (
     <div>
-      {" "}
-      <CSVReader
-        onUploadAccepted={(results) => {
-          console.log("---------------------------");
-          console.log(results);
-          console.log("---------------------------");
-          setCsvData(results.data);
-          setErrors(results.errors);
-        }}
-      >
-        {({ getRootProps, acceptedFile, ProgressBar, getRemoveFileProps }) => (
-          <div>
-            <div>
-              <div style={styles.csvReader}>
-                <button
-                  type="button"
-                  {...getRootProps()}
-                  style={styles.browseFile}
-                >
-                  Browse file
-                </button>
-                <div style={styles.acceptedFile}>
-                  {acceptedFile && acceptedFile.name}
-                </div>
-                <button {...getRemoveFileProps()} style={styles.remove}>
-                  Remove
-                </button>
-              </div>
-              <ProgressBar style={styles.progressBarBackgroundColor} />
-            </div>
-            <form>
-              <input type="file" onChange={handleOFXImport} accept=".ofx" />
-            </form>
-            <ol>
-              {csvData.map((expense, index) => (
-                <li key={index}>{expense[2]}</li>
-              ))}
-            </ol>
-          </div>
-        )}
-      </CSVReader>
+      <CSVUpload setCsvData={setCsvData} setErrors={setErrors} />
+      <OFXUpload setOfxData={setOfxData} setErrors={setErrors} />
+      <ol>
+        {csvData.length !== 0 &&
+          csvData.map((expense, index) => <li key={index}>{expense[2]}</li>)}
+        {ofxData.length !== 0 &&
+          ofxData.map((expense) => <li key={expense.FITID}>{expense.MEMO}</li>)}
+      </ol>
     </div>
   );
 }
---
+++ reactapp/src/components/import/importFilter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function importFilter(props) {
+  return <div>importFilter</div>;
+}
+
+export default importFilter;
